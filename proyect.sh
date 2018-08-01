ac9ec64bfee7a38819606c3145623df8394a0a5a
CatalinaSilva <38567419+CatalinaSilva@users.noreply.github.com>
2018-08-01 00:58:19 -0400

    PRIMERA PARTE FINISHED

--- proyect.sh
+++ proyect.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
-clear
+
 
 FECHA=`date +"%d/%m/%Y"`
+CALENDARIO=`cal`
 HORA=`date +"%H:%M"`
 NOMBRE=`uname -n`
-ESTADO=`top -bn1`
+##ESTADO=`top -bn1`
 MEMORIA=`vm_stat`
 DISCOS=`df -h`
 TIEMPO=`uptime`
+CASA=`ls $HOME`
+MBYTES=`du -h -s -m $HOME`
 
 echo
 echo "			** WELCOME $USER **"
 
-echo "La fecha de hoy es: $FECHA."
-echo "La hora actual es: $HORA."
-echo "Nombre de la maquina: $NOMBRE."
-echo "Estado de CPU: $ESTADO."
-echo "Uso de memoria: $MEMORIA."
-echo "Estado de discos: $DISCOS."
+echo "La fecha de hoy es: $FECHA. \n"
+echo "La hora actual es: $HORA. \n"
+echo "\n $CALENDARIO \n"
+echo "Nombre de la maquina: $NOMBRE.\n"
+##xecho "Estado de CPU: $ESTADO.\n"
+echo "Uso de memoria: $MEMORIA.\n"
+echo "Estado de discos: $DISCOS.\n"
 echo "Usuario: $USER."
-echo "Tiempo utilizando el equipo: $TIEMPO."
-echo ""
-escho
+echo "Tiempo utilizando el equipo: $TIEMPO.\n"
+echo "\tArchivos en su /home: \n$CASA\n"
+echo "Su /home posee : $MBYTES Mbytes"
+
