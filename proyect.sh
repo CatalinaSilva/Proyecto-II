f0cce0b18aefe9be861ba42cfe00c47e96e7271d
CatalinaSilva <38567419+CatalinaSilva@users.noreply.github.com>
2018-08-01 11:59:50 -0400

    Add files via upload

--- proyect.sh
+++ proyect.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 
@@ -6,12 +7,12 @@ FECHA=`date +"%d/%m/%Y"`
 CALENDARIO=`cal`
 HORA=`date +"%H:%M"`
 NOMBRE=`uname -n`
-##ESTADO=`top -bn1`
-MEMORIA=`vm_stat`
+ESTADO=`sar -u 1 2 | tail -1 | awk '{print $5 "%"}'`
+MEMORIA=`free -m | awk {'print $1 " " $2'}`
 DISCOS=`df -h`
 TIEMPO=`uptime`
 CASA=`ls $HOME`
-MBYTES=`du -h -s -m $HOME`
+MBYTES=`du -hsm $HOME | awk '{print $1 " MB"}'`
 
 echo
 echo "			** WELCOME $USER **"
@@ -20,11 +21,10 @@ echo "La fecha de hoy es: $FECHA. \n"
 echo "La hora actual es: $HORA. \n"
 echo "\n $CALENDARIO \n"
 echo "Nombre de la maquina: $NOMBRE.\n"
-##xecho "Estado de CPU: $ESTADO.\n"
-echo "Uso de memoria: $MEMORIA.\n"
+echo "Estado de CPU: $ESTADO .\n"
+echo "Uso de memoria: $MEMORIA MB.\n"
 echo "Estado de discos: $DISCOS.\n"
 echo "Usuario: $USER."
 echo "Tiempo utilizando el equipo: $TIEMPO.\n"
 echo "\tArchivos en su /home: \n$CASA\n"
 echo "Su /home posee : $MBYTES Mbytes"
-
